9fec4abf54868cf248ca872e80dde662893f7416
PhamAn12 <32708796+PhamAn12@users.noreply.github.com>
2019-03-28 09:36:05 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, length: { maximum: Setting.max_length_name }
+  validates :name, presence: true, length: { maximum: Settings.max_length_name }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, presence: true, length: { maximum: Setting.max_length_email },
+  validates :email, presence: true, length: { maximum: Settings.max_length_email },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: Setting.min_length_password}
+  validates :password, presence: true, length: { minimum: Settings.min_length_password}
 end
